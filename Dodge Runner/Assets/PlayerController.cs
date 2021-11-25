f04a95b45a651d289161dce3b32e6ff2824753f7
ClarkKent-eu <74181748+ClarkKent-eu@users.noreply.github.com>
2021-11-25 04:16:13 +0000

    Fixing Mechanic's Part 1
    Fixing the camera and jump mechanic

--- Dodge Runner/Assets/PlayerController.cs
+++ Dodge Runner/Assets/PlayerController.cs
@@ -6,21 +6,41 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 40.0f;
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
 
 
     // Start is called before the first frame update
     void Start()
-        
-
 
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed );
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
+
